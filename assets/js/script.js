13384f888dae47a312cf9cf26fded88f416bdf79
NicholasLysons <boylysons@icloud.com>
2023-02-07 23:16:59 -0700

    added more script and finalized some sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,19 @@
-var APIKEY ="48b65b00c9d41008bc1bb8ebf7388640"
-
-function getApi() {
-
-    var requestUrl ='https://api.openweathermap.org/data/2.5/weather?q={city}';
-
-    fetch(requestUrl)
-    .then(function(response){
-        return response.json()
+    var fetchButton = document.queryElementById('fetch-button');
+    
+    function getApi() {
+        
+        const apikey = "48b65b00c9d41008bc1bb8ebf7388640"
+        var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}'
+        fetch(requestUrl)
+        .then(function(response) {
+            return response.json();
+    })
+    .then(function (data){
+            for(var i = 0; i < data.length; i++) {
+                var listItem = document.createElement('li')
+                listItem.textContext = data[i].html_url;
+                repoList.appendChild(listItem);
+            }
     });
 }
-
-
+fetchButton.addEventListener('click', getApi);
